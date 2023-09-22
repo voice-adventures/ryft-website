58a2f3358f624026a45556fae1e89f536ab278ff
Alex Lerman <alexander.a.lerman@gmail.com>
2023-09-22 15:29:09 -0400

    adding images and parallax effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,184 @@
+"use client"
 import Image from 'next/image'
+import { Libre_Baskerville } from 'next/font/google';
+import { useEffect, useState } from 'react';
+import { Parallax } from 'react-scroll-parallax';
+import { ParallaxProvider } from 'react-scroll-parallax';
+// import background from ''
+// If loading a variable font, you don't need to specify the font weight
+const font = Libre_Baskerville({
+  subsets: ['latin'],
+  weight: '400',
+});
+
+const Hero = () => {
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const handleScroll = () => {
+    const position = window.scrollY;
+    setScrollPosition(position);
+    console.log('scroll position', position);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <div
+      style={{
+        height: '2700px',
+        maxWidth: '100vw',
+        position: 'relative',
+        alignItems: 'center',
+      }}
+    >
+      <div
+        style={{
+          position: scrollPosition > 900 ? 'relative' : 'fixed',
+          bottom: 0,
+          left: '50%',
+          transform: 'translateX(-50%)',
+          height: '100vh',
+          width: '100vw',
+          minWidth: 1300,
+          marginTop: scrollPosition > 900 ? '900px' : 0,
+          alignItems: 'center',
+        }}
+      >
+        {/* <Parallax style={{position: 'relative', height: '100vh', width: '100vw'}} speed={-1000}> */}
+        <img
+          style={{ width: '100%', position: 'absolute', bottom: 0 }}
+          src={'/assets/images/MANSION-background.png'}
+          alt="Ryft: A Timely Manor"
+        />
+        <Parallax
+          startScroll={0}
+          endScroll={900}
+          shouldAlwaysCompleteAnimation
+          style={{ position: 'absolute', bottom: '-5%' }}
+          scale={[1, 1.1]}
+        >
+          <img
+            src={'/assets/images/treeline.png'}
+            alt="Ryft: A Timely Manor"
+          />
+        </Parallax>
+        <Parallax
+          startScroll={0}
+          endScroll={900}
+          shouldAlwaysCompleteAnimation
+          style={{ position: 'absolute', bottom: 0 }}
+          scale={[1, 1.4]}
+        >
+          <img
+            src={'/assets/images/MANSION-ground.png'}
+            alt="Ryft: A Timely Manor"
+          />
+        </Parallax>
+        <Parallax
+          startScroll={0}
+          endScroll={900}
+          shouldAlwaysCompleteAnimation
+          style={{ width: '60%', position: 'absolute', bottom: '1%', right: 0 }}
+          scale={[1, 1.5]}
+          translateX={['-150px', '-75px']}
+        >
+          <img
+            src={'/assets/images/lamppost.png'}
+            alt="Ryft: A Timely Manor"
+          />
+        </Parallax>
+        <Parallax
+          startScroll={0}
+          endScroll={900}
+          shouldAlwaysCompleteAnimation
+          style={{ width: '45%', position: 'absolute', bottom: '10%' }}
+          scale={[1, 1.6]}
+          translateX={['50px', '-800px']}
+          translateY={['0px', '-200px']}
+        >
+          <img
+            src={'/assets/images/MANSION-left-tree.png'}
+            alt="Ryft: A Timely Manor"
+          />
+        </Parallax>
+        <Parallax
+          startScroll={0}
+          endScroll={900}
+          shouldAlwaysCompleteAnimation
+          style={{
+            width: '40%',
+            position: 'absolute',
+            bottom: '15%',
+            right: 0,
+          }}
+          scale={[1, 1.6]}
+          translateX={['-50px', '800px']}
+          translateY={['0px', '-200px']}
+        >
+          <img
+            src={'/assets/images/MANSION-right-tree.png'}
+            alt="Ryft: A Timely Manor"
+          />
+        </Parallax>
+        <Parallax
+          style={{ width: '100%', position: 'absolute', top: '25%' }}
+          shouldAlwaysCompleteAnimation
+          startScroll={600}
+          endScroll={900}
+          opacity={[0, 1]}
+        >
+          <div
+            style={{
+              alignItems: 'center',
+              width: '100%',
+              position: 'absolute',
+              top: '50%',
+            }}
+          >
+            <div className="pb-6">
+              <div className="logo-wrapper">
+                <img
+                  className="logo"
+                  src={'/assets/images/RYFT_Logo_White.png'}
+                  alt="Ryft: A Timely Manor"
+                />
+              </div>
+              <div className="">
+                <div className="line" />
+                <div
+                  className={`ryft-part part-i text-center text-2xl ${font.className}`}
+                >
+                  Part XVII
+                </div>
+              </div>
+              <div
+                className={`ryft-subtitle text-center text-3xl  ${font.className}`}
+              >
+                AN ENDLESS MANOR
+              </div>
+            </div>
+          </div>
+        </Parallax>
+        {/* </Parallax> */}
+      </div>
+    </div>
+  );
+};
+
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className="flex min-h-screen flex-col items-center">
+      <ParallaxProvider>
+        <Hero />
+      </ParallaxProvider>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   )
 }
