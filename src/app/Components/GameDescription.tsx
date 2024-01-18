95d53ae7b5c750868f2d904b644b9fe08e457577
Alex Lerman <alexander.a.lerman@gmail.com>
2024-01-17 21:05:32 -0800

    adding trailer to website

--- src/app/Components/GameDescription.tsx
+++ src/app/Components/GameDescription.tsx
@@ -1,4 +1,5 @@
 import Copyright from "./Copyright"
+import Trailer from "./Trailer"
 
 const mainCopy = "Welcome to the Nexus Manor. A place that exists outside of time —and in between your ears. RYFT is an audio-based adventure game played with speech commands. Featuring a full voice cast, challenging puzzles, and a compelling narrative, this mystery game is powered by the world's greatest graphics engine: your imagination."
 const subHeaderCopy = "Navigate freely. Pick-up and combine items to solve puzzles. Chat with an eccentric cast of characters as you attempt to solve the mystery of the manor without ever lifting a finger."
@@ -27,6 +28,7 @@ const GameDescription = () => {
                 </div>
                 </div>
                 <div style={{width: '100%', height: '20vh'}} />
+                <Trailer />
                 <Copyright />
             </div>
         </div>
---
+++ src/app/Components/Trailer.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react"
+import { IoIosPlayCircle } from "react-icons/io";
+
+const Trailer = () => {
+    let [showVideo, setShowVideo] = useState(false)
+    return (
+        <div className='trailer' >
+            {showVideo ? 
+                <div className="videoImageStubWrapper sm:videoImageStubWrapperLarge">
+                    <img className="videoImageStub sm:videoImageStubLarge " src={'/og.webp'} />
+                    <iframe src='https://www.youtube.com/embed/xgAXQGemVGc?autoplay=1'
+                    className="video sm:videoLarge"
+                    frameborder='0'
+                    allow='autoplay; encrypted-media'
+                    allowFullScreen
+                    title='video'/>
+ 
+                </div>
+                :
+                <div className="videoImageStubWrapper sm:videoImageStubWrapperLarge">
+                    <img className="videoImageStub sm:videoImageStubLarge " src={'/og.webp'} onClick={() => setShowVideo(true)} />
+                    <IoIosPlayCircle className="playIcon sm:playIconLarge" onClick={() => setShowVideo(true)}/>
+                </div>
+        }
+
+        </div>
+    )
+  }
+  
+  export default Trailer
+
